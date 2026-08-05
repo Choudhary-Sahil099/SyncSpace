c093d7ee82bd4dbf5b393d8c23042966e2833d37
Sahil choudhary <freedomchoudhary099@gmail.com>
2026-08-05 19:08:17 +0530

    OT basic

--- server/internal/websocket/operation.go
+++ server/internal/websocket/operation.go
@@ -1,8 +1,20 @@
 package websocket
 
+// better version update for the OT engine
 type Operation struct {
+
+	// identity
+	ID     string `json:"id"`
+	UserID string `json:"userId"`
+
+	// metadata
 	Type     string `json:"type"`
 	Position int    `json:"position"`
 	Text     string `json:"text,omitempty"`
 	Length   int    `json:"length,omitempty"`
+
+	// metaData
+	BaseVersion int64 `json:"baseVersion"`
+	Version     int64 `json:"version"`
+	Timestamp   int64 `json:"timestamp"`
 }
